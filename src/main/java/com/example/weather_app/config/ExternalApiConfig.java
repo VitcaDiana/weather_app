716e82dc18bca164fcbb08699c3b8063240a9edc
VitcaDiana <diana.ni.vitca@gmail.com>
2024-03-25 19:14:29 +0200

    weather-app

---
+++ src/main/java/com/example/weather_app/config/ExternalApiConfig.java
@@ -0,0 +1,21 @@
+package com.example.weather_app.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ExternalApiConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper (){
+        return new ObjectMapper();
+    }
+
+}
---
+++ src/main/java/com/example/weather_app/dto/CurrentWeatherDTO.java
@@ -0,0 +1,54 @@
+package com.example.weather_app.dto;
+
+import java.time.LocalDateTime;
+
+public class CurrentWeatherDTO {
+    private double temperature;
+
+
+    private double feelsLikeTemperature;
+
+    private double humidity;
+
+    private LocalDateTime date;
+
+    public CurrentWeatherDTO(double temperature, double feelsLikeTemperature, double humidity, LocalDateTime date) {
+        this.temperature = temperature;
+        this.feelsLikeTemperature = feelsLikeTemperature;
+        this.humidity = humidity;
+        this.date = date;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getFeelsLikeTemperature() {
+        return feelsLikeTemperature;
+    }
+
+    public void setFeelsLikeTemperature(double feelsLikeTemperature) {
+        this.feelsLikeTemperature = feelsLikeTemperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+}
